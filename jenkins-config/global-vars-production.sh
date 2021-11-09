7b157fb0242bd15dacea6bb87f85eb41aeb1f75c
Vanessa Valderrama <vvalderrama@linuxfoundation.org>
2021-11-09 12:37:15 -0600

    Chore: Configure Jenkins
    Configuring global vars and OpenStack cloud
    
    Signed-off-by: Vanessa Valderrama <vvalderrama@linuxfoundation.org>
    Change-Id: I9f5ee469cb649db799af99bf17bed99100fe25a8

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,6 @@
+GERRIT_URL=https://gerrit.tungsten.io/gerrit
+GIT_URL=ssh://tungsten-jobbuilder@gerrit.tungsten.io:29418
+JENKINS_HOSTNAME=vex-yul-tungsten-jenkins-prod-1
+NEXUS_URL=""
+S3_BUCKET=""
+SILO=production
