eb82d1aca42c162acf273b485d9d2ae128fa3152
Vanessa Valderrama <vvalderrama@linuxfoundation.org>
2022-02-11 10:16:22 -0600

    CI: Configure S3 logs
    Issue-ID: RELENG-4062
    Signed-off-by: Vanessa Valderrama <vvalderrama@linuxfoundation.org>
    Change-Id: I98040a811f5f79d41627fbbefc5e57dc3678d09a

--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -2,5 +2,5 @@ GERRIT_URL=https://gerrit.tungsten.io/gerrit
 GIT_URL=ssh://tungsten-jobbuilder@gerrit.tungsten.io:29418
 JENKINS_HOSTNAME=vex-yul-tungsten-jenkins-sandbox-1
 NEXUS_URL=""
-S3_BUCKET=""
+S3_BUCKET=tungsten-logs-s3-cloudfront-index
 SILO=sandbox
