2d3b15826f96df016e99120e90daa258c177bfd1
Vanessa Valderrama <vvalderrama@linuxfoundation.org>
2022-02-25 11:59:26 -0600

    Chore: Add CDN_URL for S3 logs
    Signed-off-by: Vanessa Valderrama <vvalderrama@linuxfoundation.org>
    Change-Id: Iab8e1d68067e67ed88ac413fe4191bd284505dda

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -3,4 +3,5 @@ GIT_URL=ssh://tungsten-jobbuilder@gerrit.tungsten.io:29418
 JENKINS_HOSTNAME=vex-yul-tungsten-jenkins-prod-1
 NEXUS_URL=""
 S3_BUCKET=tungsten-logs-s3-cloudfront-index
+CDN_URL=s3-logs.tungesten.io
 SILO=production
